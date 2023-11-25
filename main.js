ac817caa6583cdf6a6af9251b6fb14c9c0799ceb
andrewye0128 <b0936486128@gmail.com>
2023-11-25 17:59:49 +0800

    finish login and signup layout and api connection

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-import 'tailwindcss/tailwind.css'
+// import "tailwindcss/tailwind.css";
+// import "./src/css/tailwind.css"; 
+import headers from "./src/js/header.js";
+import {
+  authSigns,
+  authLogins,
+  authAdminLogins,
+  others,
+} from "./src/api/auth.js";
+
+// 漢堡排事件
+headers.menu.addEventListener("click", headers.menuDisplayToggle);
+headers.close.addEventListener("click", headers.closeDisplayToggle);
+
+// 導覽列滑動改變
+headers.scrollChange()
+
+// 註冊、登入頁面
+// 確認目前在哪個頁面上
+if (authSigns.signForm) {
+  // console.log(`目前在註冊頁面`);
+
+  // 註冊事件
+  authSigns.signAddEvent();
+
+} else if (authLogins.loginForm) {
+  // console.log(`目前在前台登入頁面`);
+
+  // 登入事件
+  authLogins.loginEvent();
+
+} else if (authAdminLogins.adminLoginForm) {
+  // console.log(`目前在後台登入頁面`);
+  authAdminLogins.loginEvent();
+}
+
+
+// 如果已登入，將『登入/註冊』，改為『登出』
+others.toggleLoginName();
+
+
+// 登出點擊事件
+others.loginName.addEventListener("click", others.logOut);
