bcdad74d036960d8a2a5ae5907df2c5c5df11178
AlannaYL <a04810254@gmail.com>
2023-12-02 15:05:23 +0800

    [upload]更新預約頁面的icon

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,7 @@
-
 // 抓取節點
 const menu = document.querySelector('#menu')
 const links = document.querySelector('#links')
 const close = document.querySelector('#close')
-
 
 // 點擊menu函式
 function menuDisplayToggle() {
@@ -15,32 +13,29 @@ function closeDisplayToggle() {
   links.style.width = '0vw'
 }
 
-
-const header = document.querySelector("#header");
-let prevScrollPos = window.scrollY;
+const header = document.querySelector('#header')
+let prevScrollPos = window.scrollY
 
 const scrollChange = () => {
-  window.addEventListener("scroll", () => {
-    const currentScrollPos = window.scrollY;
+  window.addEventListener('scroll', () => {
+    const currentScrollPos = window.scrollY
 
     if (prevScrollPos > currentScrollPos) {
       // 向上滾動，顯示導航欄
-      header.style.top = "0";
+      header.style.top = '0'
     } else {
       // 向下滾動，隱藏導航欄
-      header.style.top = "-85px";
+      header.style.top = '-85px'
     }
 
-    prevScrollPos = currentScrollPos;
-  });
+    prevScrollPos = currentScrollPos
+  })
 
   // 頁面載入時顯示導航欄
-  window.addEventListener("DOMContentLoaded", () => {
-    header.style.top = "0";
-  });  
+  window.addEventListener('DOMContentLoaded', () => {
+    header.style.top = '0'
+  })
 }
-
-
 
 // heder全部內容一次打包導出
 const headers = {
@@ -51,7 +46,7 @@ const headers = {
   prevScrollPos,
   menuDisplayToggle,
   closeDisplayToggle,
-  scrollChange,
-};
+  scrollChange
+}
 
-export default headers;
+export default headers
